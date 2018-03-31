bb7c3290c0a30eecbde331e9a53691c1b565589c
pwentz <patrick.w.wentz@gmail.com>
2018-03-31 16:25:43 -0500

    - create function to get aggregate trade chat data for given user

--- src/Queries/TradeChat.hs
+++ src/Queries/TradeChat.hs
@@ -1,14 +1,24 @@
 module Queries.TradeChat where
 
 import           Config               (App)
+import           Control.Applicative
+import           Data.Int             (Int64)
+import qualified Data.Map             as Map
 import           Database.Esqueleto
 import qualified Database.Persist.Sql as Sql
 import qualified Db.Main              as Db
+import           Models.Message
 import           Models.Offer
 import           Models.Trade
 import           Models.TradeChat
 import           Models.User
+import qualified Queries.Message      as MsgQuery
 import           Utils                (sHead)
+
+data ChatData =
+  ChatData { recipient :: Int64
+           , messages  :: [Sql.Entity Message]
+           } deriving (Eq, Show)
 
 findByTrade :: Sql.Key Trade -> App (Maybe (Sql.Entity TradeChat))
 findByTrade tradeKey = sHead <$> foundTradeChat
@@ -35,3 +45,60 @@ findByUser = (fmap . fmap) unValue . getTradeChats
             on (trades ^. TradeId ==. tradeChats ^. TradeChatTradeId)
             where_ (users ^. UserId ==. val userKey)
             return (tradeChats ^. TradeChatId)
+
+findChatData :: Sql.Key User -> App (Map.Map Int64 ChatData)
+findChatData userKey =
+   foldr
+    (\tradeChatKey ->
+      liftA2
+        (Map.insert (Sql.fromSqlKey tradeChatKey))
+        (liftA2
+          (ChatData . Sql.fromSqlKey . recipient)
+          (findRecipients tradeChatKey)
+          (MsgQuery.getMessages tradeChatKey)
+        )
+    )
+    (return Map.empty) =<< findByUser userKey
+     where
+         recipient :: (Sql.Key User, Sql.Key User) -> Sql.Key User
+         recipient (acceptedUserKey, exchangeUserKey)
+             | userKey == acceptedUserKey = exchangeUserKey
+             | otherwise = acceptedUserKey
+
+findRecipients :: Sql.Key TradeChat -> App (Sql.Key User, Sql.Key User)
+findRecipients tradeChatKey = do
+  (acceptedUserKey:exchangeUserKey:_) <- getUsers tradeChatKey
+  return (unValue acceptedUserKey, unValue exchangeUserKey)
+  where
+      getUsers :: Sql.Key TradeChat -> App [Value (Sql.Key User)]
+      getUsers tcKey =
+        Db.run $
+          select $
+            from $ \(tradeChats `InnerJoin` trades `InnerJoin` offers `InnerJoin` users) -> do
+              on (users ^. UserId ==. offers ^. OfferUserId)
+              on
+                (offers ^. OfferId ==. trades ^. TradeAcceptedOfferId ||.
+                 offers ^. OfferId ==. trades ^. TradeExchangeOfferId)
+              on (trades ^. TradeId ==. tradeChats ^. TradeChatTradeId)
+              where_ (tradeChats ^. TradeChatId ==. val tcKey)
+              return (users ^. UserId)
+
+findAllChatData :: Sql.Key User -> App [(Sql.Entity TradeChat, Sql.Entity Trade, Sql.Entity Offer, Sql.Entity User, Sql.Entity Message)]
+findAllChatData userKey =
+  let
+    chatMessages =
+      from $ \(messages) -> do
+          orderBy [asc (messages ^. MessageCreatedAt)]
+          return messages
+  in
+  Db.run $
+    select $
+      from $ \(tradeChats `InnerJoin` trades `InnerJoin` offers `InnerJoin` users) -> do
+        on (users ^. UserId ==. offers ^. OfferUserId)
+        on
+          (offers ^. OfferId ==. trades ^. TradeAcceptedOfferId ||.
+           offers ^. OfferId ==. trades ^. TradeExchangeOfferId)
+        on (trades ^. TradeId ==. tradeChats ^. TradeChatTradeId)
+        orderedMessages <- chatMessages
+        where_ (users ^. UserId ==. val userKey)
+        return (tradeChats, trades, offers, users, orderedMessages)
--- test/Queries/TradeChatSpec.hs
+++ test/Queries/TradeChatSpec.hs
@@ -9,6 +9,12 @@ import           Queries.TradeChat
 import qualified SpecHelper             as Spec
 import           Test.Hspec
 import           Test.QuickCheck
+import qualified Data.Map as Map
+import Models.User
+import Models.Message
+import Data.Int (Int64)
+import Data.Maybe (fromJust)
+import Config (App)
 
 spec :: Spec
 spec =
@@ -52,3 +58,61 @@ spec =
             tradeChats <- findByUser currentUserKey
             return (tradeChats, tradeChat1Key, tradeChat2Key)
           tradeChats `shouldBe` [tradeChat1Key, tradeChat2Key]
+      context "findRecipient" $
+        it "finds the users in a given chat" $ \config -> do
+          (recipientKeys, currentUserKey, user2Key) <- Spec.runAppToIO config $ do
+            time <- liftIO getCurrentTime
+            photoKey <- Spec.createPhoto "cat.png" time
+            currentUserKey <- Spec.createUser "ed" "griswold" "eg@yahoo.com" "grissy1" "pass" Nothing Nothing time
+            user2Key <- Spec.createUser "fred" "johnson" "fred@gmail.com" "fredd" "password" Nothing Nothing time
+            user3Key <- Spec.createUser "bill" "johnson" "bill@gmail.com" "billy" "password" Nothing Nothing time
+            categoryKey <- Spec.createCategory "tutor" time
+            currentUserOfferKey <- Spec.createOffer currentUserKey categoryKey photoKey "chemistry" 1 time
+            user2OfferKey <- Spec.createOffer user2Key categoryKey photoKey "physics" 1 time
+            user3OfferKey <- Spec.createOffer user3Key categoryKey photoKey "calculus" 1 time
+            trade1Key <- Spec.createTrade currentUserOfferKey user2OfferKey False time
+            tradeChat1Key <- Spec.createTradeChat trade1Key time
+            recipientKeys <- findRecipients tradeChat1Key
+            return (recipientKeys, currentUserKey, user2Key)
+          recipientKeys `shouldBe` (currentUserKey, user2Key)
+      context "findChatData" $
+        it "gets chat data for a given user" $ \config ->
+          let
+            extractMessage :: Sql.Key Message -> App (Sql.Entity Message)
+            extractMessage msgKey = do
+              mbMsg <- Db.run (Sql.get msgKey)
+              return $ fromJust (Sql.Entity msgKey <$> mbMsg)
+            expected :: (Sql.Key TradeChat, Sql.Key User, [Sql.Key Message]) -> (Sql.Key TradeChat, Sql.Key User, [Sql.Key Message]) -> App (Map.Map Int64 ChatData)
+            expected chatData1 chatData2 =
+              foldr
+                (\(tradeChatKey, userKey, messageKeys) acc -> do
+                  messages <- traverse extractMessage messageKeys
+                  Map.insert (Sql.fromSqlKey tradeChatKey) (ChatData (Sql.fromSqlKey userKey) messages) <$> acc
+                 )
+                 (return Map.empty)
+                 [chatData1, chatData2]
+          in do
+          (chatData, expectedRes) <- Spec.runAppToIO config $ do
+            time <- liftIO getCurrentTime
+            photoKey <- Spec.createPhoto "cat.png" time
+            currentUserKey <- Spec.createUser "ed" "griswold" "eg@yahoo.com" "grissy1" "pass" Nothing Nothing time
+            user2Key <- Spec.createUser "fred" "johnson" "fred@gmail.com" "fredd" "password" Nothing Nothing time
+            user3Key <- Spec.createUser "bill" "johnson" "bill@gmail.com" "billy" "password" Nothing Nothing time
+            categoryKey <- Spec.createCategory "tutor" time
+            currentUserOfferKey <- Spec.createOffer currentUserKey categoryKey photoKey "chemistry" 1 time
+            user2OfferKey <- Spec.createOffer user2Key categoryKey photoKey "physics" 1 time
+            user3OfferKey <- Spec.createOffer user3Key categoryKey photoKey "calculus" 1 time
+            trade1Key <- Spec.createTrade currentUserOfferKey user2OfferKey False time
+            tradeChat1Key <- Spec.createTradeChat trade1Key time
+            trade2Key <- Spec.createTrade user3OfferKey currentUserOfferKey False time
+            tradeChat2Key <- Spec.createTradeChat trade2Key time
+            trade3Key <- Spec.createTrade user2OfferKey user3OfferKey False time
+            tradeChat3Key <- Spec.createTradeChat trade3Key time
+            message1Key <- Spec.createMessage tradeChat1Key currentUserKey "yo fred" time
+            message2Key <- Spec.createMessage tradeChat2Key currentUserKey "yo bill" time
+            message3Key <- Spec.createMessage tradeChat3Key user2Key "hey bill, it's me fred." time
+            message4Key <- Spec.createMessage tradeChat1Key user2Key "hello, ed" time
+            chatData <- findChatData currentUserKey
+            expectedRes <- expected (tradeChat1Key, user2Key, [message1Key, message4Key]) (tradeChat2Key, user3Key, [message2Key])
+            return (chatData, expectedRes)
+          chatData `shouldBe` expectedRes
