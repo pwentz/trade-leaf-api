d8e20d268fe7540975dbb4870135f6215d3a2cb9
pwentz <patrick.w.wentz@gmail.com>
2018-04-22 15:17:43 -0500

    - fix coords of other users to be within radius of current location - encrypt password before passing to createUser function

--- seed/Main.hs
+++ seed/Main.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import           Api.Auth                    (encodePassword)
 import           Config                      (App, Config (..),
                                               Environment (Development),
                                               getConfig, makePool, runApp)
@@ -7,7 +8,9 @@ import           Control.Exception           (throwIO)
 import           Control.Monad.Except        (runExceptT)
 import           Control.Monad.IO.Class      (liftIO)
 import           Control.Monad.Reader        (runReaderT)
+import qualified Data.ByteString.Char8       as BS
 import           Data.Coords
+import           Data.Maybe                  (fromJust)
 import           Data.Time                   (getCurrentTime)
 import qualified Database.Persist.Postgresql as Pg
 import qualified Db.Main                     as Db
@@ -50,7 +53,8 @@ runAppToIO config app = do
 
 
 seedDb :: Config -> IO ()
-seedDb config =
+seedDb config = do
+  encodedPassword <- BS.unpack . fromJust <$> encodePassword "password"
   runAppToIO config $ do
     time <- liftIO getCurrentTime
     photoKey <- Db.createPhoto "" time
@@ -59,19 +63,19 @@ seedDb config =
     manInRainPhotoKey <- Db.createPhoto "http://res.cloudinary.com/tradeleaf/image/upload/v1511638661/rain-watercolor_kunesi.jpg" time
     woodworkingCategoryKey <- Db.createCategory "woodworking" time
     artCategoryKey <- Db.createCategory "decorative art" time
-    currentUserKey <- Db.createUser "pat" "wentz" "pat@yahoo.com" "pwentz" "password" Nothing (Just $ Coords 41.938132 (-87.642753)) time
+    currentUserKey <- Db.createUser "pat" "wentz" "pat@yahoo.com" "pwentz" encodedPassword Nothing (Just $ Coords 41.938132 (-87.642753)) time
     currentUserOffer1Key <- Db.createOffer currentUserKey artCategoryKey photoKey "some painting" 999 time
     currentUserRequest1Key <- Db.createRequest currentUserOffer1Key artCategoryKey "looking for nice painting i can hang in office" time
     {-| 6 miles from currentUser -}
-    user2Key <- Db.createUser "Fred" "Johnson" "fjohn@gmail.com" "freddyjohn" "password" Nothing (Just $ Coords 41.858210 (-87.651700)) time
+    user2Key <- Db.createUser "Fred" "Johnson" "fjohn@gmail.com" "freddyjohn" encodedPassword Nothing (Just $ Coords 37.785834 (-122.406417)) time
     user2OfferKey <- Db.createOffer user2Key artCategoryKey waterColorPaintingPhotoKey "water color 30x40 painting" 999 time
     user2OfferRequestKey <- Db.createRequest user2OfferKey artCategoryKey "animal painting for kid" time
     {-| 14 miles from currentUser -}
-    user3Key <- Db.createUser "Crack" "Jackson" "crackjack@gmail.com" "crackjack1" "password" Nothing (Just $ Coords 41.734517 (-87.674043)) time
+    user3Key <- Db.createUser "Crack" "Jackson" "crackjack@gmail.com" "crackjack1" encodedPassword Nothing (Just $ Coords 37.785834 (-122.406417)) time
     user3OfferKey <- Db.createOffer user3Key artCategoryKey fingerPaintingPupPhotoKey "finger painting dog with lots of colors" 999 time
     user3OfferRequestKey <- Db.createRequest user3OfferKey artCategoryKey "looking for a large painting" time
     {-| 9 miles from currentUser -}
-    user4Key <- Db.createUser "Millie" "Bobby Brown" "bobby@brown.com" "milliebob" "password" Nothing (Just $ Coords 41.804575 (-87.671359)) time
+    user4Key <- Db.createUser "Millie" "Bobby Brown" "bobby@brown.com" "milliebob" encodedPassword Nothing (Just $ Coords 37.785834 (-122.406417)) time
     user4OfferKey <- Db.createOffer user4Key artCategoryKey manInRainPhotoKey "man in rain - watercolor" 999 time
     user4OfferRequestKey <- Db.createRequest user4OfferKey woodworkingCategoryKey "looking for a wooden ship" time
     currentUserOffer2Key <- Db.createOffer currentUserKey woodworkingCategoryKey photoKey "wooden battleship" 999 time
